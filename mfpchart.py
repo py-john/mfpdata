5305d30078c26ae3e0677b16abe6598c55a16501
py-john <pyjohn3@gmail.com>
2018-05-26 23:53:55 -0700

    Formatting and clean up

--- mfpchart.py
+++ mfpchart.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-import os
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib import style
-from datetime import date, timedelta
 
 PACKAGE_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -18,30 +15,10 @@ df['fat'] *= 9
 df['macro_cals'] = df[['fat', 'protein', 'carbohydrates']].sum(axis=1)
 df['cal_diff'] = df['calories'] - df['macro_cals']
 
-entrydf = df[['calories', 'macro_cals', 'entries']].copy()
-
 edict = {}
-edictlists = {}
-for day in entrydf['entries']:
-    for k, v in day.items():
-        k = ','.join(k.split(',')[:-1])
-        edict[k] = v
-
-for day in entrydf['entries']:
-    for k, v in day.items():
-        k = ','.join(k.split(',')[:-1])
-        templist =  edictlists.get(k, [])
-        templist.append(v)
-        edictlists[k] = templist[:]
-
-edict = {}
-counts = {}
-for date, day in entrydf['entries'].items():
-    for k, v in day.items():
-        k = ','.join(k.split(',')[:-1])
-        tmplist =  counts.get(k, [])
-        tmplist.append(date)
-        counts[k] = tmplist
-        edict[k] = v
-        edict[k]['dates'] = tmplist
-        edict[k]['occurences'] = len(tmplist)
+for day in df['entries']:
+    for food_item, macros in day.items():
+        name = ','.join(food_item.split(',')[:-1])
+        templist = edict.get(name, [])
+        templist.append(macros)
+        edict[name] = templist[:]
